4d18d79a89db12d5d9a625d40e817db41153959f
Cam Wheatley <cameron_2000@live.co.uk>
2024-04-29 23:19:21 +0000

    add meta to comment model

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,12 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+    
+    def __str__(self):
+        return f"{self.title} | Written by {self.author}"
+
 
 class Comments(models.Model):
     post = models.ForeignKey(
@@ -28,3 +34,9 @@ class Comments(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment: {self.body} by {self.author}"
