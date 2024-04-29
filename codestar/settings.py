6cfa446d345f4873e88555c8d403642b53c84898
Cam Wheatley <cameron_2000@live.co.uk>
2024-04-29 18:10:50 +0000

    create post modal

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-cameronjames-djangoblog-74cnmgueoe7.ws-us110.gitpod.io',
+ALLOWED_HOSTS = ['8000-cameronjames-djangoblog-mv0o0745tra.ws-us110.gitpod.io',
                  '.herokuapp.com']
 
 
@@ -90,6 +90,10 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
